9ce13c0b7fee9ce6bc2156834ba0f6f3acf9c646
sayalikeche <kechesayali11@gmail.com>
2021-11-10 18:31:43 +0530

    [Sayali Welcome to Employew Wage Comptation]

---
+++ EmployeeWageeCompition/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EmployeeWageeCompition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to computaion program");
+                Console.ReadLine();
+
+
+        }
+    }
+}
