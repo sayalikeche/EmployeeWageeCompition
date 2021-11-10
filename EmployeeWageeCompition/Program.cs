bec4bd5d3a0b567cec1a323acbf5f9571d63302e
sayalikeche <kechesayali11@gmail.com>
2021-11-10 21:27:20 +0530

    [Sayali Adding DayMonth]

--- EmployeeWageeCompition/Program.cs
+++ EmployeeWageeCompition/Program.cs
@@ -7,28 +7,34 @@ namespace EmployeeAttendance
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 2;
 
         static void Main(string[] args)
         {
             int empHrs = 0;
             int empWage = 0;
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
-            {
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                default:
+            totalEmpWage = 0;
+            for(int day=0; day <NUM_OF_WORKING_DAYS; day++)
+            { 
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                   case IS_PART_TIME:
+                       empHrs = 4;
+                       break;
+                   case IS_FULL_TIME:
+                       empHrs = 8;
+                       break;
+                   default:
                     empHrs = 0;
                     break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
+                Console.WriteLine("EmpWage : " + empWage);
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("EmpWage : " + empWage);
+            Console.WriteLine("Total Emp Wage : " + totalEmpWage);
         }
-    }
 
-}
+    }
