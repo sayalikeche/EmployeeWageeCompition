dd9a51c58b2e34ad949663c8275963a52c27726d
sayalikeche <kechesayali11@gmail.com>
2021-11-10 18:51:12 +0530

    [Sayali Adding Employee Attendace]

--- EmployeeWageeCompition/Program.cs
+++ EmployeeWageeCompition/Program.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 
-namespace EmployeeWageeCompition
+namespace EmployeeAttendance
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("welcome to computaion program");
-                Console.ReadLine();
-
-
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int result = random.Next(0, 2);
+            if (result == IS_FULL_TIME)
+                Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee is Absent");
         }
     }
 }
